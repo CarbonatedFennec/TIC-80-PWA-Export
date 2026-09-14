6f9df077c33f446a8156ce4ba62fd57792ca5300
CarbonatedFennec <239501465+CarbonatedFennec@users.noreply.github.com>
2026-09-14 18:47:13 -0500

    Finished worker.js

--- src/worker.js
+++ src/worker.js
@@ -1,7 +1,28 @@
-self.addEventListener("install", event => {
-   console.log("Service worker installed");
+const resources = [
+    "/",
+    "/cart.tic",
+    "/index.html",
+    "/manifest.json",
+    "/tic80.js",
+    "/tic80.wasm",
+    "/worker.js"
+];
+
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches
+            .open("tic-80")
+            .then((cache) => {
+                cache.addAll(resources);
+            })
+            .catch((err) => console.error(err))
+    );
 });
 
-self.addEventListener("activate", event => {
-   console.log("Service worker activated");
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+      caches.match(e.request).then((res) => {
+        return res || fetch(e.request);
+      })
+    );
 });
